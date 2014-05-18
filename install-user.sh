bfeed88637f4b9aba594a01e377c8e4d1376ae70
root <root@server.antesocial.fr>
2014-05-18 13:12:40 +0200

    bug correction in install-user when installing from a distant dir

--- install-user.sh
+++ install-user.sh
@@ -7,8 +7,8 @@ elif [ ! ${DIR:0:1} == "/" ]; then
     DIR=$(pwd)/$(dirname "$0")
 fi
 
-for f in homedot/*; do
-    ln -vfs "$DIR/$f" $HOME/.$(basename $f)
+for f in $DIR/homedot/*; do
+    ln -vfs $f $HOME/.$(basename $f)
 done
 
 if [ -d $HOME/.kde4/share/kde4/services ]; then
